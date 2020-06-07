9cc689319204a5b84b92f98f5403eafe0b1da603
scrappy-strategy-4846 <tomlai8882000@hotmail.com>
2020-06-07 04:27:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-    new_array << array[counter]
+    new_array << array[counter] **2
     
     counter += 1 
  end 
